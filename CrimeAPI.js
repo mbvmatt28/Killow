f1305c123e593c7aee3326a946afd5db2c77ce99
Doug Admin <dougadmin28@DatafileTechnologies.local>
2019-04-09 11:25:38 -0500

    playing with API on index.html page

--- CrimeAPI.js
+++ CrimeAPI.js
@@ -1,8 +1,8 @@
 $.ajax({
-    url: "https://data.kcmo.org/resource/nyg5-tzkz.json",
+    url: "https://data.kcmo.org/resource/nyg5-https://data.kcmo.org/resource/nyg5-tzkz.json?$query=SELECT description, location WHERE description LIKE '%HOMICIDE%'.json",
     type: "GET",
     data: {
-        "$limit": 5000,
+        "$limit": 99999,
         "$$app_token": "TRCaZxpAuvpOqRAYYPamdgcBO"
     }
 }).done(function(data) {
